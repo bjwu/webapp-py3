1d6c5d04c6b53f62fc0f1011b5d08cc4d7180ca5
wubijia <bjwu@zju.edu.cn>
2019-01-03 22:30:38 +0800

    Add find(), save()

--- www/orm.py
+++ www/orm.py
@@ -161,6 +161,21 @@ class Model(dict, metaclass = ModelMetaclass):
                 setattr(self, key, value)
         return value
 
+    @classmethod
+    async def find(cls, pk):
+        'find object by primary key'
+        rs = await select('%s where `%s`=?' %(cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warning('failed to insert record: affected rows: %s' % rows)
+
 
 # ORM
 # 创建实例： user = User(id = 123, name = 'Jack')
@@ -174,12 +189,4 @@ class User(Model):
 
     id = IntegerField(primary_key = True)
     name = StringField()
-
-
-
-
-
-
-
-
 
